3791e61066d93f31e17bc154fc740fef5fc93f47
noname306 <ya.sitora11@gmail.com>
2024-12-24 20:38:04 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let ism = prompt ("Enter your name:");
+let surname = prompt ("Enter your surname:");
+let age = +prompt ("Enter your age:");
+let adres = prompt ("Enter your address:");
+let favsubject = prompt ("Enter your favourite subject:");
+console.log(ism, surname, age, adres, favsubject);
+
